4244185491d9ea6f2b7a820773a8f35d905ca4e8
gustt <16337193+gustt@user.noreply.gitee.com>
2025-12-10 21:34:33 +0800

    学习响应式数据驱动

--- vue/ref/demo3/ref-demo/src/App.vue
+++ vue/ref/demo3/ref-demo/src/App.vue
@@ -1,30 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { 
+    ref,
+    onMounted  // 挂在之后
+   } from 'vue';  // 响应式API 可以将数据包装成响应式对象
+  
+  const users = ref([
+    
+  ]);
+  onMounted(() => {
+    console.log("页面已经挂在完成");
+    fetch('http://localhost:3000/users')
+      .then(res => res.json())
+      .then(data => {
+        users.value = data;
+      })
+  })
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{user.id}}</td>
+        <td>{{user.name}}</td>
+        <td>{{user.email}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
